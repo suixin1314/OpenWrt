ed7b1095874e9fd0331b73cba61e0cfdf94818ce
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-04-23 00:41:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@ echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.d
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
-echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
+#echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
