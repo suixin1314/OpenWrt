f67fbec66ce1ee7926564f7bf8e4bb71cb53fd75
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-04-27 22:35:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,3 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feed
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
 #echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
-git clone -b v1.0 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
