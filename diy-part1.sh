843dfb13ca82184eed6f8495d4481925b3e9c065
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-05-16 00:46:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,9 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 echo 'src-git openwrt-passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
 #echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
-echo 'src-git ua3f https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
-echo 'src-git openClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
