ea38abb9108bb60afaeb83e2279e11d2be8de2eb
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-05-16 00:50:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,6 @@ echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>fe
 echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+
+#添加passwall的包
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
