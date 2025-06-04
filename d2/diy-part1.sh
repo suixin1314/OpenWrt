c6090892ae435eb959bca73d9d9b0fba1d8a2880
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-06-04 22:17:30 +0800

    Update diy-part1.sh

--- d2/diy-part1.sh
+++ d2/diy-part1.sh
@@ -28,5 +28,6 @@ echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.con
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 
 rm -rf feeds/packages/net/msd_lite
+rm -rf package/feeds/luci/luci-app-msd_lite
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
