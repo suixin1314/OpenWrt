f51639b766227238f26a45e4eb2a62b99617a2d1
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-06-05 22:42:33 +0800

    Update diy-part1.sh

--- ipq60xx/diy-part1.sh
+++ ipq60xx/diy-part1.sh
@@ -17,8 +17,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo " " >> feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small.git' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git' >> feeds.conf.default
 #echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >> feeds.conf.default
 #echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >> feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
