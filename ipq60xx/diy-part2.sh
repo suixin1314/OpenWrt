5a4fb0ff1f7dc697381951d5df5fd8f91ad53e7f
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-06-06 20:19:37 +0800

    Update diy-part2.sh

--- ipq60xx/diy-part2.sh
+++ ipq60xx/diy-part2.sh
@@ -39,12 +39,7 @@ function git_sparse_clone() {
 }
 
 
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
+
 
 
 
