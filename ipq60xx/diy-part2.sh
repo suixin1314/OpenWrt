738a0314ef0dfb6d344d65105d56c637644b8daf
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-06-10 01:07:09 +0800

    Update diy-part2.sh

--- ipq60xx/diy-part2.sh
+++ ipq60xx/diy-part2.sh
@@ -28,20 +28,10 @@ rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
 
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
 
 #rust编译问题
 sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
-sed -i 's/python3-netifaces/python3-netifaces-py3/' \
-  package/feeds/packages/mqttled/Makefile
+
 
 
 
