d48a945cd42f9d33ab37c214e474c792314eaf1a
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-06-12 01:19:06 +0800

    Update diy-part2.sh

--- ipq60xx/diy-part2.sh
+++ ipq60xx/diy-part2.sh
@@ -33,24 +33,6 @@ rm -rf feeds/packages/shadow
 
 #rust编译问题
 sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
-# 修复 mqttled 依赖
-sed -i 's/python3-netifaces/netifaces-python3/' \
-  package/feeds/packages/mqttled/Makefile
-
-# 修复 shadow 依赖
-sed -i 's/libxcrypt/libxcrypt-full/' \
-  package/feeds/packages/shadow/Makefile
-
-# 添加 libxcrypt-full 依赖声明
-sed -i '/define Package\/shadow\/install/i \	+libxcrypt-full \\' \
-  package/feeds/packages/shadow/Makefile
-  
-cat >> .config << 'EOF'
-CONFIG_PACKAGE_netifaces-python3=y
-CONFIG_PACKAGE_libxcrypt-full=y
-CONFIG_PACKAGE_python3-paho-mqtt=y
-CONFIG_PACKAGE_libmosquitto-ssl=y
-EOF
 
 
 
