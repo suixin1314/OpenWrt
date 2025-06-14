2d401ad08e99b4c8be205169b88aad586f032e38
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-06-14 23:43:40 +0800

    Update diy-part1.sh

--- ipq60xx/diy-part1.sh
+++ ipq60xx/diy-part1.sh
@@ -27,8 +27,8 @@ echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.con
 #添加passwall的包
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 
 
