7da462ffc13fa28b62a95098d85361032e0bf8ed
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-07-26 20:36:31 +0800

    Update diy-part1.sh

--- ipq60xx/diy-part1.sh
+++ ipq60xx/diy-part1.sh
@@ -23,6 +23,7 @@ echo 'src-git small https://github.com/kenzok8/small.git' >> feeds.conf.default
 #echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >> feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 #echo 'src-git qmodem https://github.com/FUjr/QModem.git' >> feeds.conf.default
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 
 #添加passwall的包
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
