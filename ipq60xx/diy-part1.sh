d886e45d992b279de7adbc4fce6e5ab3d9f6b08e
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-12-09 18:00:32 +0800

    Update diy-part1.sh

--- ipq60xx/diy-part1.sh
+++ ipq60xx/diy-part1.sh
@@ -24,7 +24,7 @@ echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> f
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 #echo 'src-git qmodem https://github.com/FUjr/QModem.git' >> feeds.conf.default
 git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
-git clone https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
+git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
 
 #添加passwall的包
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
